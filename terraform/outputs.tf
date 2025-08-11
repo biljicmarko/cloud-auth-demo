66ea5decbd4d30dcdedc9aafa565500804044154
Biljic Marko <m.biljic@a1.rs>
2025-08-11 17:01:29 +0200

    Fix bug

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "azure_public_ip" {
-  description = "Public IPv4 of the VM"
-  value       = module.network.public_ip_address
+  value = module.network.public_ip
 }
 
 output "azure_public_fqdn" {
-  description = "Public FQDN of the public IP"
-  value       = module.network.public_fqdn
+  value = module.network.public_fqdn
 }
